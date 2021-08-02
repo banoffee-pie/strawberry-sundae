e99c04d0b6f4f56d27c22ee73f0785e33187cfda
Salman Khan <salmankhan@xmos.com>
2021-08-02 08:54:44 +0100

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const clangExtensions: Array<string> = [
+  'c',
+  'h',
+  'cpp',
+  'java',
+  'json',
+  'js',
+];
+export const pythonExtensions: Array<string> = ['py', 'py'];
+export const requiredBinaries: Array<string> = [
+  'python',
+  'pip',
+  'node',
+  'clang-format',
+];
